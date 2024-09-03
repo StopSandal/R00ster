caff6c3945b6c6a5b4c7eaa1095cd8a21893f67d
Кирилл Солдатенко <soldatenko.kirillas@gmail.com>
2024-09-03 19:31:51 +0300

    feat: add EF to project

---
+++ EF/R00sterContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using R00ster.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R00ster.EF
+{
+    internal class R00sterContext : DbContext
+    {
+        internal DbSet<Joke> Jokes { get; set; }
+        public R00sterContext(DbContextOptions<R00sterContext> options) : base(options)
+        {
+        }
+    }
+}
