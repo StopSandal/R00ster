7a02bc6d14db57bc971076f7641b206ea68f23bb
Кирилл Солдатенко <soldatenko.kirillas@gmail.com>
2024-09-04 22:01:20 +0300

    Fix: added auto migrations

--- EF/R00sterContext.cs
+++ EF/R00sterContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using R00ster.Entities;
+using System.IO;
 
 namespace R00ster.EF
 {
@@ -8,6 +10,10 @@ namespace R00ster.EF
         internal DbSet<Joke> Jokes { get; set; }
         public R00sterContext(DbContextOptions<R00sterContext> options) : base(options)
         {
+            if (Database.GetPendingMigrations().Any())
+            {
+                Database.Migrate();
+            }
         }
     }
 }
